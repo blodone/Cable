53361ce31bd3992e41ff936e7caffaba18ef0d0a
magillos <kerown@gmail.com>
2025-03-29 09:43:08 +0000

    added version info and new version check

--- Cable.py
+++ Cable.py
@@ -18,7 +18,7 @@ from PyQt6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
                              QCheckBox, QSystemTrayIcon, QMenu)
 
 # --- Application Version ---
-APP_VERSION = "0.7.4"
+APP_VERSION = "0.7.3"
 # -------------------------
 
 class AutostartManager:
